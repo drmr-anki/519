3235818ca54218a368d2a93934f8be422a77f71d
ruixia2@illinois.edu <xiarui@xiaruideMacBook-Pro.local>
2017-12-16 17:56:22 -0600

    new

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-function selectCountry(val){
-	alert(val)
+function selectCountry(country){
+	// alert(country)
+
+	document.getElementById('country').innerHTML = country;
+
+	//find the clubs in that country and replace this array
+	var clubs = ['Beijing','Shanghai','Najing','Shandong'];
+
+	document.getElementById('club').innerHTML = writeClubButton(clubs);
+
+}
+
+
+function writeClubButton(clubs){
+    // caution: drop the "new Array" part or it won't work!
+
+    var printThis = "";
+    for(var i = 0; i < clubs.length; i++){
+        printThis += '<button class="ui blue basic button">'+clubs[i]+'</button>';
+    }
+    return printThis; // <-- to be printed to the div
 }
