20236f6709535f7be4a588b9c0aacb6f1a4a503a
ruixia2@illinois.edu <xiarui@xiaruideMacBook-Pro.local>
2017-12-17 23:46:23 -0600

    12/17

--- js/chart.js
+++ js/chart.js
@@ -1,453 +1,469 @@
-var myChart = echarts.init(document.getElementById('main'));
-// Data
-// Season => Year
-var Season = ["10/11","11/12","12/13","13/14","14/15","15/16","16/17"];
-var radarData = [];
-var radarDataAvg = [];
-var maxData = 3000;
-var weekMaxData = [3000,3000,3000,3000,3000,3000,3000];
-var weekLineData = [355,280,801,2693,550,1783,2899];
-
-//7 years with 6 element each year
-radarData.unshift([49,33,330,11649,400,379]);
-radarData.unshift([38,45,416,12864,314,429]);
-radarData.unshift([66,41,426,12329,331,483]);
-radarData.unshift([41,29,516,11593,378,465]);
-radarData.unshift([55,35,466,11725,328,497]);
-radarData.unshift([46,43,457,11117,336,470]);
-radarData.unshift([37,40,377,11020,269,441]);
-
-radarDataAvg.unshift([38,45,416,12864,314,429]);
-radarDataAvg.unshift([66,41,426,12329,331,483]);
-radarDataAvg.unshift([41,29,516,11593,378,465]);
-radarDataAvg.unshift([55,35,466,11725,328,497]);
-radarDataAvg.unshift([46,43,457,11117,336,470]);
-radarDataAvg.unshift([37,40,377,11020,269,441]);
-radarDataAvg.unshift([40,31,407,11230,321,412]);
-
-// Set color
-var color = {
-linearYtoG: {
-	type: 'linear',
-	x: 0,
-	y: 0,
-	x2: 1,
-	y2: 1,
-	colorStops: [{
-		offset: 0,
-		color: '#f5b44d'
-	}, {
-		offset: 1,
-		color: '#28f8de'
-	}]
-},
-linearGtoB: {
-	type: 'linear',
-	x: 0,
-	y: 0,
-	x2: 1,
-	y2: 0,
-	colorStops: [{
-		offset: 0,
-		color: '#43dfa2'
-	}, {
-		offset: 1,
-		color: '#28f8de'
-	}]
-},
-linearBtoG: {
-	type: 'linear',
-	x: 0,
-	y: 0,
-	x2: 1,
-	y2: 0,
-	colorStops: [{
-		offset: 0,
-		color: '#1c98e8'
-	}, {
-		offset: 1,
-		color: '#28f8de'
-	}]
-},
-areaBtoG: {
-	type: 'linear',
-	x: 0,
-	y: 0,
-	x2: 0,
-	y2: 1,
-	colorStops: [{
-		offset: 0,
-		color: 'rgba(35,184,210,.2)'
-	}, {
-		offset: 1,
-		color: 'rgba(35,184,210,0)'
-	}]
-}
-};
-
-var option = {
-title: {
-	text: 'Analysis of Player Transfer',
-	textStyle: {
-		color: '#fff',
-		fontSize: 24,
-		fontWeight: 'normal'
-	},
-	subtext: "Hao Wang, Anqi Mao",
-	subtextStyle: {
-		color: '#fff',
-		fontSize: 16,
-	},
-	top: 50,
-	left: 80
-},
-legend: {
-	top: 220,
-	left: 80,
-	orient: 'vertical',
-	itemGap: 15,
-	itemWidth: 12,
-	itemHeight: 12,
-	data: ['Last Season', 'Current Season'],
-	textStyle: {
-		color: '#fff',
-		fontSize: 14,
-	},
-},
-tooltip: {
-	trigger: 'item'
-},
-radar: {
-	center: ['68%', '27%'],
-	radius: '40%',
-	name: {
-		color: '#fff'
-	},
-	splitNumber: 8,
-	axisLine: {
-		lineStyle: {
-			color: color.linearYtoG,
-			opacity: .6
-		}
-	},
-	splitLine: {
-		lineStyle: {
-			color: color.linearYtoG,
-			opacity: .6
-		}
-	},
-	splitArea: {
-		areaStyle: {
-			color: '#fff',
-			opacity: .1,
-			shadowBlur: 25,
-			shadowColor: '#000',
-			shadowOffsetX: 0,
-			shadowOffsetY: 5,
-		}
-	},
-	indicator: [{
-		name: 'Goals',
-		max: 70
-	}, {
-		name: 'Fumble',
-		max: 60
-	}, {
-		name: 'Shoot',
-		max: 600
-	}, {
-		name: 'Passing',
-		max: 14000
-	},{
-		name: 'Threatening pass',
-		max: 500
-	},{
-		name: 'Steals',
-		max: 600
-	}]
-},
-grid: {
-	left: 90,
-	right: 80,
-	bottom: 40,
-	top: '60%',
-},
-xAxis: {
-	type: 'category',
-	position: 'bottom',
-	axisLine: true,
-	axisLabel: {
-		color: 'rgba(255,255,255,.8)',
-		fontSize: 12
-	},
-	data: Season,
-},
-yAxis: [{
-	name: 'Cost(Mill. €)',
-	nameLocation: 'end',
-	nameGap: 24,
-	position: 'left',
-	nameTextStyle: {
-		color: 'rgba(255,255,255,.5)',
-		fontSize: 14
-	},
-	max: maxData,
-	splitNumber: 4,
-
-	axisLine: {
-		lineStyle: {
-			opacity: 0
-		}
-	},
-	splitLine: {
-		show: true,
-		lineStyle: {
-			color: '#fff',
-			opacity: .1
-		}
-	},
-	axisLabel: {
-		color: 'rgba(255,255,255,.8)',
-		fontSize: 12
-
+function drawChart(radarData, radarDataPrev, Season, costData, rankData){
+	let main = document.getElementById("main");
+	let existInstance = echarts.getInstanceByDom(main);
+	if (existInstance) {
+    if (true) {
+        echarts.dispose(existInstance);
+    }
 	}
-},{
-	name: 'Rank',
-	nameLocation: 'start',
-	nameGap: 24,
-	position: 'right',
-	nameTextStyle: {
-		color: 'rgba(255,255,255,.5)',
-		fontSize: 14
-	},
-	max: 15,
-	min: 1,
-	inverse: true,
-	splitNumber: 4,
+	let myChart = echarts.init(main);
+	// echarts.init(document.getElementById('main')).dispose();
 
-	axisLine: {
-		lineStyle: {
-			opacity: 0
-		}
-	},
-	splitLine: {
-		show: true,
-		lineStyle: {
-			color: '#fff',
-			opacity: .1
-		}
-	},
-	axisLabel: {
-		color: 'rgba(255,255,255,.8)',
-		fontSize: 12
+	// Data
+	// Season => Year
+	// var Season = ["10/11","11/12","12/13","13/14","14/15","15/16","16/17"];
+	// var radarData = [];
+	var radarDataPrev = radarData;
+	var maxDataSingle = 25;
+	var maxData = [maxDataSingle,maxDataSingle,maxDataSingle,maxDataSingle,maxDataSingle,maxDataSingle,maxDataSingle];
+	// var costData = [355,280,801,2693,550,1783,2899];
+	// var rankData =[];
 
+	//7 years with 9 element each year
+	// radarData.unshift([49,33,330,11649,400,379]);
+	// radarData.unshift([38,45,416,12864,314,429]);
+	// radarData.unshift([66,41,426,12329,331,483]);
+	// radarData.unshift([41,29,516,11593,378,465]);
+	// radarData.unshift([55,35,466,11725,328,497]);
+	// radarData.unshift([46,43,457,11117,336,470]);
+	// radarData.unshift([37,40,377,11020,269,441]);
+
+	// radarDataPrev.unshift([38,45,416,12864,314,429]);
+	// radarDataPrev.unshift([66,41,426,12329,331,483]);
+	// radarDataPrev.unshift([41,29,516,11593,378,465]);
+	// radarDataPrev.unshift([55,35,466,11725,328,497]);
+	// radarDataPrev.unshift([46,43,457,11117,336,470]);
+	// radarDataPrev.unshift([37,40,377,11020,269,441]);
+	// radarDataPrev.unshift([40,31,407,11230,321,412]);
+
+	// Set color
+	var color = {
+	linearYtoG: {
+		type: 'linear',
+		x: 0,
+		y: 0,
+		x2: 1,
+		y2: 1,
+		colorStops: [{
+			offset: 0,
+			color: '#f5b44d'
+		}, {
+			offset: 1,
+			color: '#28f8de'
+		}]
+	},
+	linearGtoB: {
+		type: 'linear',
+		x: 0,
+		y: 0,
+		x2: 1,
+		y2: 0,
+		colorStops: [{
+			offset: 0,
+			color: '#43dfa2'
+		}, {
+			offset: 1,
+			color: '#28f8de'
+		}]
+	},
+	linearBtoG: {
+		type: 'linear',
+		x: 0,
+		y: 0,
+		x2: 1,
+		y2: 0,
+		colorStops: [{
+			offset: 0,
+			color: '#1c98e8'
+		}, {
+			offset: 1,
+			color: '#28f8de'
+		}]
+	},
+	areaBtoG: {
+		type: 'linear',
+		x: 0,
+		y: 0,
+		x2: 0,
+		y2: 1,
+		colorStops: [{
+			offset: 0,
+			color: 'rgba(35,184,210,.2)'
+		}, {
+			offset: 1,
+			color: 'rgba(35,184,210,0)'
+		}]
 	}
-}],
-series: [{
-	name: 'statistics',
-	type: 'radar',
-	symbolSize: 0,
-	data: [{
-		value: radarDataAvg[6],
-		name: 'Last Season',
-		itemStyle: {
-			normal: {
-				color: '#f8d351',
-			}
+	};
+
+	var option = {
+	title: {
+		text: 'Analysis of Player Transfer',
+		textStyle: {
+			color: '#fff',
+			fontSize: 24,
+			fontWeight: 'normal'
 		},
-		lineStyle: {
-			normal: {
-				opacity: 0,
-			}
+		subtext: "Hao Wang, Anqi Mao",
+		subtextStyle: {
+			color: '#fff',
+			fontSize: 16,
 		},
-		areaStyle: {
-			normal: {
-				color: '#f8d351',
-				shadowBlur: 25,
-				shadowColor: 'rgba(248,211,81,.3)',
-				shadowOffsetX: 0,
-				shadowOffsetY: -10,
-				opacity: 1
-			}
-		},
-	}, {
-		value: radarData[6],
-		name: 'Current Season',
-		itemStyle: {
-			normal: {
-				color: '#43dfa2',
-			}
-		},
-		lineStyle: {
-			normal: {
-				opacity: 0,
-			}
-		},
-		areaStyle: {
-			normal: {
-				color: color.linearGtoB,
-				shadowBlur: 15,
-				shadowColor: 'rgba(0,0,0,.2)',
-				shadowOffsetX: 0,
-				shadowOffsetY: 5,
-				opacity: .8
-			}
-		},
-	}]
-}, {
-	name: 'Cost History',
-	type: 'line',
-	smooth: true,
-	symbol: 'emptyCircle',
-	symbolSize: 8,
-	itemStyle: {
-		normal: {
-			color: '#fff'
-		}
+		top: 50,
+		left: 80
 	},
-	lineStyle: {
-		normal: {
-			color: color.linearBtoG,
-			width: 3
-		}
-	},
-	areaStyle: {
-		normal: {
-			color: color.areaBtoG,
-		}
-	},
-	data: weekLineData,
-	lineSmooth: true,
-	markLine: {
-		silent: true,
-		data: [{
-			type: 'average',
-			name: 'Average of Cost'
-		}],
-		precision: 0,
-		label: {
-			normal: {
-				formatter: 'Avg Cost: \n {c}'
-			}
+	legend: {
+		top: 220,
+		left: 80,
+		orient: 'vertical',
+		itemGap: 15,
+		itemWidth: 12,
+		itemHeight: 12,
+		data: ['Last Season', 'Current Season'],
+		textStyle: {
+			color: '#fff',
+			fontSize: 14,
 		},
-		lineStyle: {
-			normal: {
-				color: 'rgba(248,211,81,.7)'
-			}
-		}
 	},
 	tooltip: {
-		position: 'top',
-		formatter: '{c} Mill. €',
-		backgroundColor: 'rgba(28,152,232,.2)',
-		padding: 6
-	}
-}, {
-	name: 'Rank',
-	type: 'line',
-	smooth: true,
-	data: [5,10,2,4,3,14,6],
-	yAxisIndex:1,
-	lineStyle: {
-		normal: {
-			color: color.linearGtoB,
-			width: 3
-		}
-	}
-},{
-	name: 'background',
-	type: 'bar',
-	itemStyle: {
-		normal: {
-			show: true,
-			color: '#000',
-			opacity: 0
-		}
+		trigger: 'item'
 	},
-	silent: true,
-	barWidth: '50%',
-	data: weekMaxData,
-	animation: false
-}, {
-	name: 'background',
-	type: 'bar',
-	itemStyle: {
-		normal: {
-			show: true,
-			color: '#000',
-			opacity: .1
-		}
-	},
-	silent: true,
-	barWidth: '50%',
-	barGap: 0,
-	data: weekMaxData,
-	animation: false
-}],
-backgroundColor: '#383546',
-};
-
-// Clcik event
-myChart.on('click', function(params) {
-if (params.componentType === 'series' && params.seriesType === 'line') {
-
-	var dataIndex = params.dataIndex;
-	myChart.setOption({
-		series: [
-		{
-	name: 'statistics',
-	type: 'radar',
-	symbolSize: 0,
-	data: [{
-		name: 'Last Season',
-		value: radarDataAvg[dataIndex],
-		itemStyle: {
-			normal: {
-				color: '#f8d351',
+	radar: {
+		center: ['68%', '27%'],
+		radius: '40%',
+		name: {
+			color: '#fff'
+		},
+		splitNumber: 8,
+		axisLine: {
+			lineStyle: {
+				color: color.linearYtoG,
+				opacity: .6
 			}
 		},
-		lineStyle: {
-			normal: {
-				opacity: 0,
+		splitLine: {
+			lineStyle: {
+				color: color.linearYtoG,
+				opacity: .6
 			}
 		},
-		areaStyle: {
-			normal: {
-				color: '#f8d351',
+		splitArea: {
+			areaStyle: {
+				color: '#fff',
+				opacity: .1,
 				shadowBlur: 25,
-				shadowColor: 'rgba(248,211,81,.3)',
-				shadowOffsetX: 0,
-				shadowOffsetY: -10,
-				opacity: 1
-			}
-		},
-	}, {
-		name: 'Current Season',
-		value: radarData[dataIndex],
-		itemStyle: {
-			normal: {
-				color: '#43dfa2',
-			}
-		},
-		lineStyle: {
-			normal: {
-				opacity: 0,
-			}
-		},
-		areaStyle: {
-			normal: {
-				color: color.linearGtoB,
-				shadowBlur: 15,
-				shadowColor: 'rgba(0,0,0,.2)',
+				shadowColor: '#000',
 				shadowOffsetX: 0,
 				shadowOffsetY: 5,
-				opacity: .8
 			}
 		},
+		indicator: [{
+			name: 'GA',
+			max: 70
+		}, {
+			name: 'GS',
+			max: 60
+		}, {
+			name: 'Passing',
+			max: 600
+		}, {
+			name: 'Points',
+			max: 14000
+		},{
+			name: 'Possession',
+			max: 500
+
+		},{
+			name: 'Shooting',
+			max: 500
+
+		}]
+	},
+	grid: {
+		left: 90,
+		right: 80,
+		bottom: 40,
+		top: '60%',
+	},
+	xAxis: {
+		type: 'category',
+		position: 'bottom',
+		axisLine: true,
+		axisLabel: {
+			color: 'rgba(255,255,255,.8)',
+			fontSize: 12
+		},
+		data: Season,
+	},
+	yAxis: [{
+		name: 'Cost(Mill. €)',
+		nameLocation: 'end',
+		nameGap: 24,
+		position: 'left',
+		nameTextStyle: {
+			color: 'rgba(255,255,255,.5)',
+			fontSize: 14
+		},
+		max: maxDataSingle,
+		splitNumber: 4,
+
+		axisLine: {
+			lineStyle: {
+				opacity: 0
+			}
+		},
+		splitLine: {
+			show: true,
+			lineStyle: {
+				color: '#fff',
+				opacity: .1
+			}
+		},
+		axisLabel: {
+			color: 'rgba(255,255,255,.8)',
+			fontSize: 12
+
+		}
+	},{
+		name: 'Rank',
+		nameLocation: 'start',
+		nameGap: 24,
+		position: 'right',
+		nameTextStyle: {
+			color: 'rgba(255,255,255,.5)',
+			fontSize: 14
+		},
+		max: 17,
+		min: 1,
+		inverse: true,
+		splitNumber: 4,
+
+		axisLine: {
+			lineStyle: {
+				opacity: 0
+			}
+		},
+		splitLine: {
+			show: true,
+			lineStyle: {
+				color: '#fff',
+				opacity: .1
+			}
+		},
+		axisLabel: {
+			color: 'rgba(255,255,255,.8)',
+			fontSize: 12
+
+		}
+	}],
+	series: [{
+		name: 'statistics',
+		type: 'radar',
+		symbolSize: 0,
+		data: [{
+			value: radarDataPrev[5],
+			name: 'Last Season',
+			itemStyle: {
+				normal: {
+					color: '#f8d351',
+				}
+			},
+			lineStyle: {
+				normal: {
+					opacity: 0,
+				}
+			},
+			areaStyle: {
+				normal: {
+					color: '#f8d351',
+					shadowBlur: 25,
+					shadowColor: 'rgba(248,211,81,.3)',
+					shadowOffsetX: 0,
+					shadowOffsetY: -10,
+					opacity: 1
+				}
+			},
+		}, {
+			value: radarData[5],
+			name: 'Current Season',
+			itemStyle: {
+				normal: {
+					color: '#43dfa2',
+				}
+			},
+			lineStyle: {
+				normal: {
+					opacity: 0,
+				}
+			},
+			areaStyle: {
+				normal: {
+					color: color.linearGtoB,
+					shadowBlur: 15,
+					shadowColor: 'rgba(0,0,0,.2)',
+					shadowOffsetX: 0,
+					shadowOffsetY: 5,
+					opacity: .8
+				}
+			},
+		}]
+	}, {
+		name: 'Cost History',
+		type: 'line',
+		smooth: true,
+		symbol: 'emptyCircle',
+		symbolSize: 8,
+		itemStyle: {
+			normal: {
+				color: '#fff'
+			}
+		},
+		lineStyle: {
+			normal: {
+				color: color.linearBtoG,
+				width: 3
+			}
+		},
+		areaStyle: {
+			normal: {
+				color: color.areaBtoG,
+			}
+		},
+		data: costData,
+		lineSmooth: true,
+		markLine: {
+			silent: true,
+			data: [{
+				type: 'average',
+				name: 'Average of Cost'
+			}],
+			precision: 0,
+			label: {
+				normal: {
+					formatter: 'Avg Cost: \n {c}'
+				}
+			},
+			lineStyle: {
+				normal: {
+					color: 'rgba(248,211,81,.7)'
+				}
+			}
+		},
+		tooltip: {
+			position: 'top',
+			formatter: '{c} Mill. €',
+			backgroundColor: 'rgba(28,152,232,.2)',
+			padding: 6
+		}
+	}, {
+		name: 'Rank',
+		type: 'line',
+		smooth: true,
+		data: rankData,
+		yAxisIndex:1,
+		lineStyle: {
+			normal: {
+				color: color.linearGtoB,
+				width: 3
+			}
+		}
+	},{
+		name: 'background',
+		type: 'bar',
+		itemStyle: {
+			normal: {
+				show: true,
+				color: '#000',
+				opacity: 0
+			}
+		},
+		silent: true,
+		barWidth: '50%',
+		data: maxData,
+		animation: false
+	}, {
+		name: 'background',
+		type: 'bar',
+		itemStyle: {
+			normal: {
+				show: true,
+				color: '#000',
+				opacity: .1
+			}
+		},
+		silent: true,
+		barWidth: '50%',
+		barGap: 0,
+		data: maxData,
+		animation: false
+	}],
+	backgroundColor: '#383546',
+	};
+
+	// Clcik event
+	myChart.on('click', function(params) {
+	if (params.componentType === 'series' && params.seriesType === 'line') {
+
+		var dataIndex = params.dataIndex;
+		myChart.setOption({
+			series: [
+			{
+		name: 'statistics',
+		type: 'radar',
+		symbolSize: 0,
+		data: [{
+			name: 'Last Season',
+			value: radarDataPrev[dataIndex],
+			itemStyle: {
+				normal: {
+					color: '#f8d351',
+				}
+			},
+			lineStyle: {
+				normal: {
+					opacity: 0,
+				}
+			},
+			areaStyle: {
+				normal: {
+					color: '#f8d351',
+					shadowBlur: 25,
+					shadowColor: 'rgba(248,211,81,.3)',
+					shadowOffsetX: 0,
+					shadowOffsetY: -10,
+					opacity: 1
+				}
+			},
+		}, {
+			name: 'Current Season',
+			value: radarData[dataIndex],
+			itemStyle: {
+				normal: {
+					color: '#43dfa2',
+				}
+			},
+			lineStyle: {
+				normal: {
+					opacity: 0,
+				}
+			},
+			areaStyle: {
+				normal: {
+					color: color.linearGtoB,
+					shadowBlur: 15,
+					shadowColor: 'rgba(0,0,0,.2)',
+					shadowOffsetX: 0,
+					shadowOffsetY: 5,
+					opacity: .8
+				}
+			},
+		}]
 	}]
-}]
-	})
+		})
+	}
+	});
+	// Pass value to chart
+	myChart.setOption(option);
+
+
 }
-});
-// Pass value to chart
-myChart.setOption(option);
--- js/main.js
+++ js/main.js
@@ -1,39 +1,81 @@
+var data_global;
+var clubs;
+
 function selectCountry(country){
 	// alert(country)
 
 	document.getElementById('country').innerHTML = country;
 
 	//find the clubs in that country and replace this array
-	var clubs = ['Beijing','Shanghai','Najing','Shandong'];
 
-	document.getElementById('club').innerHTML = writeClubButton(clubs);
 	var path ="tempCSLData.json"
-	var data = readJSON(path);
-	console.log(data);
-	// console.log("hhhh");
+
+	readJSON(path).then((data) => {
+		var response = JSON.parse(data)
+		var data = response.data
+		data_global = data
+    //
+		console.log(data_global)
+		clubs = readUniqueClubs(data)
+		console.log(clubs);
+		// console.log("hhhh");
+		//var clubs = ['Beijing','Shanghai','Najing','Shandong'];
+
+		document.getElementById('club').innerHTML = writeClubButton(Array.from(clubs));
+	})
+
+	// console.log(data_global);
+
+
+
 
 }
 
+function readUniqueClubs(data){
+	var clubs = new Set([]);
+	for(var i = 0; i < data.length; i++){
+		clubs.add(data[i].Club);
+	}
+
+	return clubs;
+}
+
+// function saveData(data){
+// 	data_global = data;
+// }
+
 function readJSON(path) {
+	return new Promise((resolve, reject) => {
 		var data;
     var xhr = new XMLHttpRequest();
+		// xhr.overrideMimeType("application/json")
     xhr.open('GET', path, true);
-    xhr.responseType = 'blob';
-    xhr.onload = function(e) {
-      if (this.status == 200) {
-          var file = new File([this.response], 'temp');
-          var fileReader = new FileReader();
-          fileReader.addEventListener('load', function(){
-               //do stuff with fileReader.result
-							 console.log(fileReader.result.data);
-							 // data = fileReader.result;
-							 // console.log("hhhh");
-          });
-          fileReader.readAsText(file);
-      }
-    }
-    xhr.send();
-		return data;
+
+		// () => {
+    //   if (this.status == 200) {
+    //       // var file = new File([this.response], 'temp');
+    //       // var fileReader = new FileReader();
+    //       // fileReader.addEventListener('load', function(){
+    //       //      //do stuff with fileReader.result
+		// 			// 		 console.log(fileReader.result);
+		// 			// 		 // data = fileReader.result;
+		// 			// 		 // console.log("hhhh");
+    //       // });
+    //       // fileReader.readAsText(file);
+		// 			var response = JSON.parse(this.responseText);
+		// 			// console.log(data.data);
+		// 			var data = response.data;
+		// 			// console.log(data);
+		// 	    // data_global = data;
+		// 			return data
+		// 		}
+    // xhr.responseType = 'blob'
+    xhr.onload = () => resolve(xhr.responseText)
+		xhr.onerror = () => reject(xhr.statusText)
+		xhr.send();
+	})
+
+
 }
 
 function writeClubButton(clubs){
@@ -41,10 +83,58 @@ function writeClubButton(clubs){
 
     var printThis = "";
     for(var i = 0; i < clubs.length; i++){
-        printThis += '<button class="ui blue basic button">'+clubs[i]+'</button>';
+        printThis += '<button class="ui blue basic button" value ="'+clubs[i]+'" onclick = "readClubData(value)">'+clubs[i]+'</button> ';
+
     }
 
     return printThis; // <-- to be printed to the div
+}
+
+function readClubData(club){
+	var club_data = [];
+	let count = 0;
+
+	console.log(club);
+	//Find the data for club and call drawChart
+	for(var i = 0; i < data_global.length; i++){
+		if(new String(data_global[i].Club).valueOf() == new String(club).valueOf()){
+			club_data[count] = data_global[i];
+			count++;
+			// console.log("I am here");
+		}
+		// console.log(data_global[i]);
+	}
+
+	// club_data.reverse();
+	console.log(club_data);
+	var radarData = [];
+	var radarDataPrev = [];
+	var Season = [];
+	var rankData = [];
+	var costData = [];
+  //
+	for(var i = 0; i < club_data.length; i++){
+		if(i != club_data.length-1){
+			radarData.unshift([club_data[i].GA,club_data[i].GS,club_data[i].Passing,club_data[i].Points,club_data[i].Possession, club_data[i].Shooting]);
+			Season.unshift(club_data[i].Year);
+			if(club_data[i].Rank == -1){
+				rankData.unshift(17);
+			}else{
+				rankData.unshift(club_data[i].Rank);
+			}
+		}
+		if(i != 0){
+			radarDataPrev.unshift([club_data[i].GA,club_data[i].GS,club_data[i].Passing,club_data[i].Points,club_data[i].Possession, club_data[i].Shooting]);
+		}
+
+
+		costData[i] = club_data[i].Transfer_Arrival-club_data[i].Transfer_Departure;
+	}
+	console.log(radarData);
+	console.log(Season);
+
+
+	drawChart(radarData, radarDataPrev,Season, costData, rankData);
 }
 
 
